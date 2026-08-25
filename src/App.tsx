b52211a2ad8f2636279c63f92441f9d459e097c1
kevintruong-blueoc <kevin.truong.blueoc@tophat.com>
2026-08-25 23:30:32 +0700

    chore: update asset build hashes and import useMemo in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback, useRef } from 'react';
+import React, { useState, useEffect, useCallback, useRef, useMemo } from 'react';
 import { Header } from './components/Header';
 import { Navigation, TabType } from './components/Navigation';
 import { AIChatView } from './components/AIChatView';
